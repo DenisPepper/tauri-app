b53e53678a67c88f08784b77b9702c9c5b1efb0f
DenisPepper <thech@yandex.ru>
2024-04-16 23:27:29 +0300

    Добавит файлы фронтенда

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,23 @@
-import { useState } from "preact/hooks";
-import preactLogo from "./assets/preact.svg";
-import { invoke } from "@tauri-apps/api/tauri";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./components/Experience";
+import Configurator from "./components/Configurator";
+import { CustomizationProvider } from "./contexts/Customization";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <div class="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div class="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
+    <CustomizationProvider>
+      <div className="App">
+        <Canvas dpr={[1, 2]}>
+          <color attach="background" args={["#213547"]} />
+          <fog attach="fog" args={["#213547", 10, 20]} />
+          <Experience />
+        </Canvas>
+        <Configurator />
       </div>
-
-      <p>Click on the Tauri, Vite, and Preact logos to learn more.</p>
-
-      <form
-        class="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onInput={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
+    </CustomizationProvider>
   );
 }
 
